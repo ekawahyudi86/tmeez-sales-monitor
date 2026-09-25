4cc6adc49e4a654716bcd3d41dc4905fe73c22c7
ekawahyudi86 <ekawahyudi8614@gmail.com>
2026-09-25 20:13:10 +0700

    Upload Tmeez Sales Monitor V6
    Upload Tmeez Sales Monitor V6

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='tmeez-sales-v6-pwa-1';
+const APP=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;}).catch(()=>caches.match(e.request))); });
