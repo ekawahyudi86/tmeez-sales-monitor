815dc359e29916fc155a8936a63af19feda2895d
ekawahyudi86 <ekawahyudi8614@gmail.com>
2026-09-26 19:46:45 +0700

    versi 6.4.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tmeez-v6-4-4-secure-1';
+const CACHE='tmeez-v6-4-5-secure-1';
 const STATIC=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
